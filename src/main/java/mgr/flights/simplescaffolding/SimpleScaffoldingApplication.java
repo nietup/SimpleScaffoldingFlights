9851fcc20d2a036137a3f5ddd35ddedc1614d38f
nietup <22.jakub@gmail.com>
2019-10-02 21:07:24 +0200

    Initial commit

---
+++ src/main/java/mgr/flights/simplescaffolding/SimpleScaffoldingApplication.java
@@ -0,0 +1,13 @@
+package mgr.flights.simplescaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleScaffoldingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleScaffoldingApplication.class, args);
+    }
+
+}

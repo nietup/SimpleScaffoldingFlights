1d2a3a9deb58afcaa28738917a0841b65697f48d
nietup <22.jakub@gmail.com>
2019-10-07 20:51:43 +0200

    Implement CRUD controller

--- src/main/java/mgr/flights/simplescaffolding/city/CityController.java
+++ src/main/java/mgr/flights/simplescaffolding/city/CityController.java
@@ -2,11 +2,13 @@ package mgr.flights.simplescaffolding.city;
 
 import mgr.flights.simplescaffolding.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/city")
@@ -21,7 +23,40 @@ public class CityController {
     @GetMapping("/{id}")
     public ResponseEntity<City> getCityById(@PathVariable Integer id) {
         City city = cityService.getCityById(id).orElseThrow(NotFoundException::new);
-
         return ResponseEntity.ok().body(city);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<City>> getAllCities() {
+        List<City> cities = cityService.getAllCities();
+        return ResponseEntity.ok().body(cities);
+    }
+
+    @PostMapping
+    public ResponseEntity<City> createCity(@RequestBody City city) {
+        if (city.getCityId() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "CityId must be null when creating city");
+        }
+
+        City savedCity = cityService.createCity(city);
+        return ResponseEntity.ok().body(savedCity);
+    }
+
+    @PutMapping
+    public ResponseEntity<City> updateCity(@RequestBody City city) {
+        if (city.getCityId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "CityId must not be null when updating city");
+        }
+
+        City savedCity = cityService.updateCity(city)
+                .orElseThrow(NotFoundException::new);
+
+        return ResponseEntity.ok().body(savedCity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCity(@PathVariable Integer id) {
+        cityService.deleteCity(id);
+        return ResponseEntity.noContent().build();
     }
 }

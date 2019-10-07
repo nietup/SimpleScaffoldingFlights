0fd641e719f5c46e2d3e67eae68c67f9eae6d542
nietup <22.jakub@gmail.com>
2019-10-07 20:37:00 +0200

    Implement CRUD service

--- src/main/java/mgr/flights/simplescaffolding/city/CityService.java
+++ src/main/java/mgr/flights/simplescaffolding/city/CityService.java
@@ -3,6 +3,7 @@ package mgr.flights.simplescaffolding.city;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,23 @@ public class CityService {
 
     public Optional<City> getCityById(Integer id) {
         return cityRepository.findById(id);
+    }
+
+    public List<City> getAllCities() {
+        return cityRepository.findAll();
+    }
+
+    public City createCity(City city) {
+        return cityRepository.save(city);
+    }
+
+    public Optional<City> updateCity(City city) {
+        return cityRepository.findById(city.getCityId()).isPresent() ?
+                Optional.of(cityRepository.save(city)) :
+                Optional.empty();
+    }
+
+    public void deleteCity(Integer id) {
+        cityRepository.deleteById(id);
     }
 }

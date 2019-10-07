3222a3eb68df2172626421b40fe12fad87c5e36f
nietup <22.jakub@gmail.com>
2019-10-07 20:36:41 +0200

    Define persisted entity

--- src/main/java/mgr/flights/simplescaffolding/city/City.java
+++ src/main/java/mgr/flights/simplescaffolding/city/City.java
@@ -3,23 +3,31 @@ package mgr.flights.simplescaffolding.city;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "City")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class City {
     @Id
     @Column(name = "city_id")
+    @GeneratedValue
+    @NotNull
+    @NonNull
     private Integer cityId;
+
     @Column(name = "name")
+    @NotNull
+    @NonNull
     private String name;
+
     @Column(name = "time_zone")
     private Integer timeZone;
 }
